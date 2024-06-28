87d00e0f5d546a5ac77d8458bb262561eceea512
Marcos Max Forosteski da Silva <max.silva@adami.com.br>
2024-06-28 10:44:43 -0300

    adicionando angular

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const indexrouter = require('routes')
+const indexrouter = require('./routes/common')
 
 app.use('/', indexrouter);
 
